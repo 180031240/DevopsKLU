0367a93896cdc01a2ddcee78b6e6bed48a67e2b0
R G <42408254+180031240@users.noreply.github.com>
2019-11-20 15:43:31 +0530

    Update a1.cpp

--- a1.cpp
+++ a1.cpp
@@ -2,13 +2,8 @@
 using namespace std;
 int main()
 {
-    int firstNumber=5, secondNumber=9, sumOfTwoNumbers;
+  
     
-    //cout << "Enter two integers: ";
-    //cin >> firstNumber >> secondNumber;
-    // sum of two numbers in stored in variable sumOfTwoNumbers
-    sumOfTwoNumbers = firstNumber + secondNumber;
-    // Prints sum 
-    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+    cout << "Hello World!! "<<endl;     
     return 0;
 }
