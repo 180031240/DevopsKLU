209b35f82c90fd422d5e1ea9ef2378ce5ac69934
R G <42408254+180031240@users.noreply.github.com>
2019-11-20 15:29:33 +0530

    Add files via upload

---
+++ a1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int firstNumber=5, secondNumber=9, sumOfTwoNumbers;
+    
+    //cout << "Enter two integers: ";
+    //cin >> firstNumber >> secondNumber;
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    sumOfTwoNumbers = firstNumber + secondNumber;
+    // Prints sum 
+    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+    return 0;
+}
